0bd025b4d36a84a0d3bb0c2ce3c8a33a9b3ae001
keerthi.muduganti@gmail.com <kmudigan@in.ibm.com>
2021-02-09 05:06:57 -0700

    this is my first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void android()
+	{
+System.out.println("android");
+}
+	@Test
+	
+	public void iphone()
+	{
+		System.out.println("iphone");	
+	}
+}
